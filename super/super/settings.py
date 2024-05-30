1ab53a57f1f2175c8b1ff738e35f81459d91fb4a
ankitmandal24 <mandalankit244@gmail.com>
2024-05-31 00:52:15 +0530

    added_cloudinary

--- super/super/settings.py
+++ super/super/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'cloudinary_storage',
+    'cloudinary',
     'projects',
 ]
 
@@ -117,12 +119,18 @@ USE_L10N = True
 
 USE_TZ = True
 
-
+CLOUDINARY_STORAGE = {
+    'CLOUD_NAME': 'baka244',
+    'API_KEY': '476843461524239',
+    'API_SECRET': 'gAhMXfSqP9oxNUx-GaunsPE8f8Q',
+    # 'EXCLUDE_DELETE_ORPHANED_MEDIA_PATHS': ('/images/', 'images/profiles')
+}
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
 MEDIA_URL = '/images/'
+DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
 STATICFILES_DIRS = [
     BASE_DIR/ 'static'
