12ce5c16703b5931be8727cc43b5e3d966e68c59
ankitmandal24 <mandalankit244@gmail.com>
2024-05-31 13:01:51 +0530

    ready_production

--- super/build_files.sh
+++ super/build_files.sh
@@ -1,5 +1,6 @@
 echo "Build Start"
 sudo apt install python3.9-pip
+python3.9 -m pip install psycopg2-binary
 python3.9 -m pip install -r requirements.txt
 python3.9 manage.py collectstatic
 echo "Build End"
